8cb216b0942a3b3040cbcf13ffe2915a90ff00a2
Jordan Bowman <hello@jordanbowman.design>
2025-06-25 13:17:44 -0400

    Add Alpine.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import Alpine from "alpinejs"
+window.Alpine = Alpine
+Alpine.start()
 
 document.addEventListener('turbo:load', function() {
   if (window.hljs && typeof window.hljs.highlightAll === 'function') {
     window.hljs.highlightAll();
   }
 });
+
+document.addEventListener('turbo:load', () => {
+  window.Alpine && window.Alpine.initTree && window.Alpine.initTree(document.body)
+})
