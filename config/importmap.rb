407ec17522902454402acb9b4401bfd6570cce16
Jordan Bowman <hello@jordanbowman.design>
2025-06-26 15:45:35 -0400

    Install alpine turbo adapter locally

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "alpinejs" # @3.14.9
 pin "@alpinejs/collapse", to: "@alpinejs--collapse.js" # @3.14.9
-pin "alpine-turbo-drive-adapter" # @2.2.0
+pin "alpine-turbo-drive-adapter", to: "alpine-turbo-drive-adapter.min.js" # @2.2.0
