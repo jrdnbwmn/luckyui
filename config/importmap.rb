11ccb541dd88cd1eb7d8ac7214efff5350c7c47d
Jordan Bowman <hello@jordanbowman.design>
2025-06-26 11:10:33 -0400

    Properly set up alpine turbo drive adapter

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "alpinejs" # @3.14.9
 pin "@alpinejs/collapse", to: "@alpinejs--collapse.js" # @3.14.9
+pin "alpine-turbo-drive-adapter" # @2.2.0
