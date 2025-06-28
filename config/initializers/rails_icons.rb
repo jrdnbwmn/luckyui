ee37f90abaa8b0ccc6262583ec6d2388c1ab42ad
Jordan Bowman <hello@jordanbowman.design>
2025-06-27 21:33:00 -0400

    Button component

--- config/initializers/rails_icons.rb
+++ config/initializers/rails_icons.rb
@@ -5,15 +5,14 @@ RailsIcons.configure do |config|
   # config.default_variant = "outline" # Set a default variant for all libraries
 
   # Override Tabler defaults
-  config.libraries.tabler.outline.default.stroke_width = "1.5"
+  config.libraries.tabler.outline.default.stroke_width = "2"
   config.libraries.tabler.default_variant = "outline" # Set a default variant for Tabler
+  config.libraries.tabler.outline.default.css = "size-4"
+  config.libraries.tabler.filled.default.css = "size-4"
 
   # config.libraries.tabler.exclude_variants = [] # Exclude specific variants
-
   # config.libraries.tabler.regular.default.css = "size-6"
-  # config.libraries.tabler.solid.default.css = "size-6"
   # config.libraries.tabler.solid.default.data = {}
-
   # config.libraries.tabler.outline.default.css = "size-6"
   # config.libraries.tabler.outline.default.data = {}
   
@@ -25,7 +24,7 @@ RailsIcons.configure do |config|
         logos: {
           # path: "app/assets/svg/icons/logos/",
           default: {
-            css: "size-6"
+            css: "size-4"
           }
         }
 
